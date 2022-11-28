d201d4b1cca5d5a1f5a8c79c830f5e169299df22
phantom-sage <z4gamdev@gmail.com>
2022-11-28 14:30:43 +0100

    Refactor test cases  using fakery.

--- Package.swift
+++ Package.swift
@@ -12,6 +12,7 @@ let package = Package(
         .package(url: "https://github.com/vapor/fluent.git", from: "4.0.0"),
         .package(url: "https://github.com/vapor/fluent-postgres-driver.git", from: "2.0.0"),
         .package(url: "https://github.com/vapor/leaf.git", from: "4.0.0"),
+        .package(url: "https://github.com/vadymmarkov/Fakery", from: "5.0.0"),
     ],
     targets: [
         .target(
@@ -33,6 +34,7 @@ let package = Package(
         .testTarget(name: "AppTests", dependencies: [
             .target(name: "App"),
             .product(name: "XCTVapor", package: "vapor"),
+            .product(name: "Fakery", package: "Fakery")
         ])
     ]
 )
--- Tests/AppTests/ClientTests/CreateClientTests.swift
+++ Tests/AppTests/ClientTests/CreateClientTests.swift
@@ -1,5 +1,6 @@
 @testable import App
 import XCTVapor
+import Fakery
 
 
 struct ErrorFromCreateClientApi: Content {
@@ -9,12 +10,14 @@ struct ErrorFromCreateClientApi: Content {
 
 final class CreateClientTests: XCTestCase {
     var app: Application!
+    var faker: Faker!
 
     override func setUpWithError() throws {
         app = Application(.testing)
         try configure(app)
         try app.autoRevert().wait()
         try app.autoMigrate().wait()
+        faker = Faker(locale: "en")
     }
 
     override func tearDownWithError() throws {
@@ -23,12 +26,15 @@ final class CreateClientTests: XCTestCase {
 
     func testCreateClientWithValidData_clientCreated() async throws {
         try await app.test(.POST, "/api/clients", beforeRequest: { req in
-            try req.content.encode(["username": "Client username", "email": "email@email.com"])
+            try req.content.encode([
+                "username": faker.internet.username(),
+                "email": faker.internet.safeEmail()])
         }, afterResponse: { res async throws in
             XCTAssertEqual(res.status, .ok)
             let client = try res.content.decode(Client.self)
-            XCTAssertEqual(client.username, "Client username")
-            XCTAssertEqual(client.email, "email@email.com")
+            let clientFromDatabase = try await Client.query(on: app.db).first()
+            XCTAssertEqual(client.username, clientFromDatabase?.username)
+            XCTAssertEqual(client.email, clientFromDatabase?.email)
             let allClients = try await Client.query(on: app.db).all()
             XCTAssertEqual(allClients.count, 1)
         })
@@ -36,7 +42,7 @@ final class CreateClientTests: XCTestCase {
 
     func testCreateClientWithoutUsernameProvided_clientNotCreated_validationError() async throws {
         try await app.test(.POST, "/api/clients", beforeRequest: { req in
-            try req.content.encode(["email": "email@email.com"])
+            try req.content.encode(["email": faker.internet.safeEmail()])
         }, afterResponse: { res async throws in
             let response = try res.content.decode(ErrorFromCreateClientApi.self)
             XCTAssertEqual(response.reason, "username is required")
@@ -49,7 +55,7 @@ final class CreateClientTests: XCTestCase {
 
     func testCreateClientWithoutEmailAddressProvided_clientNotCreated_validationError() async throws {
         try await app.test(.POST, "/api/clients", beforeRequest: { req in
-            try req.content.encode(["username": "Client username"])
+            try req.content.encode(["username": faker.internet.username()])
         }, afterResponse: { res async throws in
             let response = try res.content.decode(ErrorFromCreateClientApi.self)
             XCTAssertEqual(response.reason, "email is required")
@@ -62,7 +68,7 @@ final class CreateClientTests: XCTestCase {
 
     func testCreateClientWithInvalidEmailAddress_clientNotCreated_validationError() async throws {
         try await app.test(.POST, "/api/clients", beforeRequest: { req in
-            try req.content.encode(["username": "Client username", "email": "invalid email address"])
+            try req.content.encode(["username": faker.internet.username(), "email": "invalid email address"])
         }, afterResponse: { res async throws in
             XCTAssertEqual(res.status, .badRequest)
             let response = try res.content.decode(ErrorFromCreateClientApi.self)
@@ -74,7 +80,9 @@ final class CreateClientTests: XCTestCase {
 
     func testCreateClientWithTooLongUsername_clientNotCreated_validationError() async throws {
         try await app.test(.POST, "/api/clients", beforeRequest: { req in
-            try req.content.encode(["username": String(repeating: "too long username", count: 5000), "email": "email@email.com"])
+            try req.content.encode([
+                "username": String(repeating: "too long username", count: 5000),
+                "email": faker.internet.safeEmail()])
         }, afterResponse: { res async throws in
             XCTAssertEqual(res.status, .badRequest)
             let response = try res.content.decode(ErrorFromCreateClientApi.self)
@@ -86,7 +94,9 @@ final class CreateClientTests: XCTestCase {
 
     func testCreateClientWithTooLongEmailAddress_clientNotCreated_validationError() async throws {
         try await app.test(.POST, "/api/clients", beforeRequest: { req in
-            try req.content.encode(["username": "Client username", "email": "email@email\(String(repeating: "email", count: 500)).com"])
+            try req.content.encode([
+                "username": faker.internet.safeEmail(),
+                "email": "email@email\(String(repeating: "email", count: 500)).com"])
         }, afterResponse: { res async throws in
             let response = try res.content.decode(ErrorFromCreateClientApi.self)
             XCTAssertEqual(response.reason, "email is not a valid email address")
@@ -99,7 +109,7 @@ final class CreateClientTests: XCTestCase {
 
     func testCreateClientWithTooShortUsername_clientNotCreated_validationError() async throws {
         try await app.test(.POST, "/api/clients", beforeRequest: { req in
-            try req.content.encode(["username": "C", "email": "email@email.com"])
+            try req.content.encode(["username": "C", "email": faker.internet.safeEmail()])
         }, afterResponse: { res async throws in
             let response = try res.content.decode(ErrorFromCreateClientApi.self)
             XCTAssertEqual("username is less than minimum of 3 character(s)", response.reason)
@@ -110,13 +120,13 @@ final class CreateClientTests: XCTestCase {
     }
 
     func testCreateClientWithDuplicateUsername_clientNotCreated_validationError() async throws {
-        let client1 = Client(username: "username", email: "email@email.com")
+        let client1 = Client(username: "username", email: faker.internet.safeEmail())
         try await client1.save(on: app.db)
         let clientsCount = try await Client.query(on: app.db).count()
         XCTAssertEqual(clientsCount, 1)
 
         try await app.test(.POST, "/api/clients", beforeRequest: { req in
-            try req.content.encode(["username": "username", "email": "newemail@email.com"])
+            try req.content.encode(["username": "username", "email": faker.internet.safeEmail()])
         }, afterResponse: { res async throws in
             XCTAssertEqual(res.status, .badRequest)
             let response = try res.content.decode(ErrorFromCreateClientApi.self)
@@ -130,13 +140,13 @@ final class CreateClientTests: XCTestCase {
     }
 
     func testCreateClientWithDuplicateEmailAddress_clientNotCreated_validationError() async throws {
-        let client1 = Client(username: "username", email: "email@email.com")
+        let client1 = Client(username: faker.internet.username(), email: "email@email.com")
         try await client1.save(on: app.db)
         let clientsCount = try await Client.query(on: app.db).count()
         XCTAssertEqual(clientsCount, 1)
 
         try await app.test(.POST, "/api/clients", beforeRequest: { req in
-            try req.content.encode(["username": "Client username", "email": "email@email.com"])
+            try req.content.encode(["username": faker.internet.username(), "email": "email@email.com"])
         }, afterResponse: { res async throws in
             XCTAssertEqual(res.status, .badRequest)
             let response = try res.content.decode(ErrorFromCreateClientApi.self)
