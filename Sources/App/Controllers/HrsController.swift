2e44b8d30cc89bd3f911938b57561bfacb086358
phantom-sage <z4gamdev@gmail.com>
2022-12-01 12:15:14 +0100

    Delete hr.

--- Sources/App/Controllers/HrsController.swift
+++ Sources/App/Controllers/HrsController.swift
@@ -6,6 +6,7 @@ struct HrsController: RouteCollection {
         let hrsRoutes = routes.grouped("api", "hrs")
         hrsRoutes.post(use: createHandler)
         hrsRoutes.put(":hrId", use: updateHandler)
+        hrsRoutes.delete(":hrId", use: deleteHandler)
     }
 
     func createHandler(_ req: Request) async throws -> HrResponse {
@@ -31,6 +32,15 @@ struct HrsController: RouteCollection {
         hr.email = hrData.email
         try await hr.update(on: req.db)
         return hr
+    }
+
+    func deleteHandler(_ req: Request) async throws -> HTTPStatus {
+        guard let hr = try await Hr.find(req.parameters.get("hrId"), on: req.db) else {
+            throw Abort(.notFound, reason: "Hr with this id: \(req.parameters.get("hrId")!) not found")
+        }
+
+        try await hr.delete(on: req.db)
+        return .noContent
     }
 }
 
---
+++ Tests/AppTests/HrTests/DeleteHrTests.swift
@@ -0,0 +1,48 @@
+@testable import App
+import XCTVapor
+import Fakery
+
+final class DeleteHrTests: XCTestCase {
+    var app: Application!
+    var faker: Faker!
+
+    override func setUpWithError() throws {
+        app = Application(.testing)
+        try configure(app)
+        try app.autoRevert().wait()
+        try app.autoMigrate().wait()
+        faker = Faker(locale: "en")
+    }
+
+    override func tearDownWithError() throws {
+        app.shutdown()
+    }
+
+    func testDeleteHr_deletedFromDatabase() async throws {
+        try await Hr.createHr(on: app.db)
+        guard let hr = try await Hr.query(on: app.db).first() else { return }
+        try await app.test(.DELETE, "/api/hrs/\(hr.id!)", afterResponse: { res async throws in
+            XCTAssertEqual(res.status, .noContent)
+
+            let hrCounts = try await Hr.query(on: app.db).count()
+            XCTAssertEqual(0, hrCounts)
+        })
+    }
+
+    func testDeleteHr_notFoundInDatabase_notFoundError() async throws {
+        let randomId = faker.number.randomInt()
+        try await app.test(.DELETE, "/api/hrs/\(randomId)", afterResponse: { res async throws in
+        XCTAssertEqual(res.status, .notFound)
+            let response = try res.content.decode(ErrorFromDeleteHrApi.self)
+            XCTAssertEqual(response.reason, "Hr with this id: \(randomId) not found")
+            XCTAssertTrue(response.error)
+
+            let hrCounts = try await Hr.query(on: app.db).count()
+            XCTAssertEqual(0, hrCounts)
+        })
+    }
+}
+struct ErrorFromDeleteHrApi: Content {
+    var error: Bool
+    var reason: String
+}
