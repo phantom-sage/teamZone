a57348f33c57ca38ac5e05062e43d36c0e50c884
phantom-sage <z4gamdev@gmail.com>
2022-12-08 09:05:42 +0100

    Update task status to use database enum.

--- Sources/App/Migrations/CreateTask.swift
+++ Sources/App/Migrations/CreateTask.swift
@@ -5,11 +5,17 @@ struct CreateTask: AsyncMigration {
         try await database.schema("tasks").delete()
     }
 
-    func prepare(on database: FluentKit.Database) async throws {       
+    func prepare(on database: FluentKit.Database) async throws {      
+        let taskStatusDatabaseEnum = try await database.enum("task_status")
+        .case(TaskStatus.completed.rawValue)
+        .case(TaskStatus.failed.rawValue)
+        .case(TaskStatus.inProgress.rawValue)
+        .create()
+
         try await database.schema("tasks")
         .id()
         .field("name", .string, .required)
-        .field("status", .string, .required)
+        .field("status", taskStatusDatabaseEnum, .required)
         .field("duration", .datetime, .required)
         .field("project_id", .uuid, .required, .references("projects", "id"))
         .create()
